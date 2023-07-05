f1c33b27c1710774cd71ecf1a415f960aca13f74
tomasmaggio <tomasmaggio220@gmail.com>
2023-07-05 10:27:02 -0300

    creacion de rutas para comunicación entre componentes dentro de auth

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //rutas secundarias
+  {
+    path: 'login', component:LoginComponent},
+    {path: 'registro', component:RegistroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
