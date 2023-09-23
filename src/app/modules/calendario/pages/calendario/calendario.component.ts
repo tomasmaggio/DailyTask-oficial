caf2660226a864c0ef757db25fd330560f2f393a
tomasmaggio <tomasmaggio220@gmail.com>
2023-09-23 15:50:29 -0300

    modal importado

--- src/app/modules/calendario/pages/calendario/calendario.component.ts
+++ src/app/modules/calendario/pages/calendario/calendario.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
 //aqui se importan los plugins del fullcalendar (primero hay que instalarlos https://fullcalendar.io/docs/plugin-index)
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import esLocale from '@fullcalendar/core/locales/es';
+//modal
+import { ModalComponent } from '../../components/modal/modal.component';
+
+
 
 
 @Component({
@@ -13,23 +17,12 @@ import esLocale from '@fullcalendar/core/locales/es';
 })
 export class CalendarioComponent {
   
+  @ViewChild(ModalComponent) modal: ModalComponent;
 
-  events: any[] = [
-    {
-      title: 'Cumpleaños de Juan',
-      start: '2023-09-15'
-    },
-    {
-       title: 'Reunión de equipo',
-       start: '2023-09-10T14:00:00'
-    },
-    {
-       title: 'Salida de campo',
-       start: '2023-09-20T08:00:00'
-    } 
-  ];
 
   calendarOptions: CalendarOptions = {
+
+    
 
     initialView: 'dayGridMonth',
     plugins: [dayGridPlugin, interactionPlugin],
@@ -42,10 +35,10 @@ export class CalendarioComponent {
     eventDrop: (info) => {
       // código para actualizar fecha en BD
     },
-    
-    events: this.events
-  
   };
+
+
+  
   
   
 }
