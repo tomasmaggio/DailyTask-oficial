743af2478388a67e3d81fadc9ea5ac8037c47756
tomasin <vendogarrafapuente83@gmail.com>
2023-10-02 10:19:26 -0300

    Cambios en app routing para establecer landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,51 +3,64 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   //lazyloading
-{
-  path:'',
-  loadChildren:()=>import('./modules/auth/auth.module').then( m=> m.AuthModule)
-},
-{
-  path:'inicio',
-  loadChildren:()=>import('./modules/inicio/inicio.module').then( m=> m.InicioModule)
-},
-{
-  path:'calendario',
-  loadChildren:()=>import('./modules/calendario/calendario.module').then( m=> m.CalendarioModule)
-},
-{
-  path:'guardados',
-  loadChildren:()=>import('./modules/guardados/guardados.module').then( m=> m.GuardadosModule)
-},
-{
-  path:'notas',
-  loadChildren:()=>import('./modules/notas/notas.module').then( m=> m.NotasModule)
-},
-{
-  path:'',
-  loadChildren:()=>import('./modules/usuario/usuario.module').then( m=> m.UsuarioModule)
-},
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full',
+  },
 
-{
-  path:'bienvenida',
-  loadChildren:()=>import('./modules/bienvenida/bienvenida.module').then( m=> m.BienvenidaModule)
-},
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'inicio',
+    loadChildren: () =>
+      import('./modules/inicio/inicio.module').then((m) => m.InicioModule),
+  },
+  {
+    path: 'calendario',
+    loadChildren: () =>
+      import('./modules/calendario/calendario.module').then(
+        (m) => m.CalendarioModule
+      ),
+  },
+  {
+    path: 'guardados',
+    loadChildren: () =>
+      import('./modules/guardados/guardados.module').then(
+        (m) => m.GuardadosModule
+      ),
+  },
+  {
+    path: 'notas',
+    loadChildren: () =>
+      import('./modules/notas/notas.module').then((m) => m.NotasModule),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/usuario/usuario.module').then((m) => m.UsuarioModule),
+  },
 
-// {
-//   path: '',
-//   redirectTo: 'bienvenida', 
-//   pathMatch: 'full'
-// },
+  {
+    path: 'bienvenida',
+    loadChildren: () =>
+      import('./modules/bienvenida/bienvenida.module').then(
+        (m) => m.BienvenidaModule
+      ),
+  },
 
-
-
-
-
-
+  // {
+  //   path: '',
+  //   redirectTo: 'bienvenida',
+  //   pathMatch: 'full'
+  // },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
