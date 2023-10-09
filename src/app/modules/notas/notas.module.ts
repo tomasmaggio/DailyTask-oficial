cbccc259f0e5fcd8acbd53874ec4406026ce3462
tomasmaggio <tomasmaggio220@gmail.com>
2023-10-09 10:51:29 -0300

    cambios

--- src/app/modules/notas/notas.module.ts
+++ src/app/modules/notas/notas.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { NotasRoutingModule } from './notas-routing.module';
 import { NotasComponent } from './pages/notas/notas.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { SplitButtonModule } from 'primeng/splitbutton';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     NotasRoutingModule,
-    SharedModule
+    SharedModule,
+    SplitButtonModule
   ]
 })
 export class NotasModule { }
