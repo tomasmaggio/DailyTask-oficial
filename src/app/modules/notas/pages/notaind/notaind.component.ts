91e9a751fd9ba595e31b3e0cc3af7bcb20262508
tomasmaggio <tomasmaggio220@gmail.com>
2023-10-13 20:56:31 -0300

    logica para mostrar el desvanecimiento correctamente

--- src/app/modules/notas/pages/notaind/notaind.component.ts
+++ src/app/modules/notas/pages/notaind/notaind.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-notaind',
   templateUrl: './notaind.component.html',
   styleUrls: ['./notaind.component.css']
 })
-export class NotaindComponent implements OnInit {
+export class NotaindComponent implements AfterViewInit {
+  @ViewChild('desvanecer') desvanecer!: ElementRef<HTMLElement>;
+  @ViewChild('notaContenido') notaContenido!: ElementRef<HTMLElement>;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.actualizarDesvanecimiento();
   }
 
+  private actualizarDesvanecimiento() {
+    const style = window.getComputedStyle(this.notaContenido.nativeElement, null);
+    const viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+
+    if (this.notaContenido.nativeElement.scrollHeight > viewableHeight) {
+      // Si hay texto de más, mostrar el desvanecimiento
+      this.renderer.setStyle(this.desvanecer.nativeElement, 'display', 'block');
+    } else {
+      // Si no hay texto de más, quitar el desvanecimiento
+      this.renderer.setStyle(this.desvanecer.nativeElement, 'display', 'none');
+    }
+  }
 }
