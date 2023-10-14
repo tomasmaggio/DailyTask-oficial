c68a0a5dfe77e2215e42b857fa26506667a42712
tomasmaggio <tomasmaggio220@gmail.com>
2023-10-13 21:31:51 -0300

    ruteo para ir diseñando el modal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotasmodalComponent } from './modules/notas/pages/notasmodal/notasmodal.component';
 
 const routes: Routes = [
   //lazyloading
@@ -23,6 +24,16 @@ const routes: Routes = [
   path:'notas',
   loadChildren:()=>import('./modules/notas/notas.module').then( m=> m.NotasModule)
 },
+
+
+//ruta provisoria solo para ir testeando el modal (si estas viendo esto en la rama main, es porque me olvide de borrarla, por favor borrala!!!!!)
+{
+  path:':id',component:NotasmodalComponent
+},
+//Borrar!!!
+
+
+
 {
   path:'',
   loadChildren:()=>import('./modules/usuario/usuario.module').then( m=> m.UsuarioModule)
