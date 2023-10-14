eeb979d1243a9dc9d0112bff5d1ea77cf7ee7d4f
tomasmaggio <tomasmaggio220@gmail.com>
2023-10-13 21:25:00 -0300

    hago la importacion

--- src/app/modules/notas/notas.module.ts
+++ src/app/modules/notas/notas.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { NotasRoutingModule } from './notas-routing.module';
 import { NotasComponent } from './pages/notas/notas.component';
 import { NotaindComponent } from './pages/notaind/notaind.component';
+import { NotasmodalComponent } from './pages/notasmodal/notasmodal.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SplitButtonModule } from 'primeng/splitbutton';
 import { InputTextModule } from 'primeng/inputtext';
