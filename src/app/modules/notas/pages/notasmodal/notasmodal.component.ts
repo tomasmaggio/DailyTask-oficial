4164ca09a591ef2772f53a593c5e71250cb7c364
tomasmaggio <tomasmaggio220@gmail.com>
2023-10-13 21:24:37 -0300

    creo un nuevo componente

---
+++ src/app/modules/notas/pages/notasmodal/notasmodal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notasmodal',
+  templateUrl: './notasmodal.component.html',
+  styleUrls: ['./notasmodal.component.css']
+})
+export class NotasmodalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
