f5f58f9802b80bf3e73a1c6d87e8c2999979e492
tomasmaggio <tomasmaggio220@gmail.com>
2023-10-17 00:08:43 -0300

    creo nueva propiedad

--- src/app/modules/notas/pages/notasmodal/notasmodal.component.ts
+++ src/app/modules/notas/pages/notasmodal/notasmodal.component.ts
@@ -18,7 +18,7 @@ export class NotasmodalComponent implements OnInit {
   //Modelo de la nota
   nota: Nota;
   notaId: number;
-
+  new: boolean;
 
   constructor(private notasService: NotasService, private router: Router, private route: ActivatedRoute) { }
 
