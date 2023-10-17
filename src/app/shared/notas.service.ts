ebdc2e0bf9eafa4d914c30404c55c2c0be0d3c28
tomasmaggio <tomasmaggio220@gmail.com>
2023-10-16 21:24:29 -0300

    agrego otro metodo

--- src/app/shared/notas.service.ts
+++ src/app/shared/notas.service.ts
@@ -8,16 +8,25 @@ export class NotasService {
 
   notas: Nota[] = new Array<Nota>();
 
-constructor() { }
+  constructor() { }
 
-//metodos para administrar los datos que están dentro
-get(id: number) {
-  return this.notas[id];
-}
+    //metodos para administrar los datos que están dentro
+    get(id: number) {
+      return this.notas[id];
+    }
 
-getId(nota: Nota) {
-  return this.notas.indexOf(nota);
-}
+    getId(nota: Nota) {
+      return this.notas.indexOf(nota);
+    }
+
+    add(nota: Nota) {
+      //este metodo agregará una nota al arreglo de notas y va a devolver el id de la nota
+      //cuando el id = index
+      let newLength = this.notas.push(nota);
+      let index = newLength - 1;
+      return index
+    }
+
 
 
 }
