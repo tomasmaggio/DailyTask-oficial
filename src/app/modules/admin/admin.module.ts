ae68246fb6223523678f674ba6a7f21b9dc0fee9
tomasmaggio <tomasmaggio220@gmail.com>
2023-11-06 09:15:34 -0300

    correcciones en la ruta

---
+++ src/app/modules/admin/admin.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './pages/admin/admin.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    AdminComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    SharedModule
+  ]
+})
+export class AdminModule { }
