907a73d184b13e87d7022e156cbb667590e8ebd7
tomasmaggio <tomasmaggio220@gmail.com>
2023-11-28 19:26:18 -0300

    me suscribo al servicio para recibir eventos

--- src/app/modules/calendario/components/modal/modal.component.ts
+++ src/app/modules/calendario/components/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { event } from 'jquery';
 import { SharedDataService } from 'src/app/shared/shared-data.service';
 
 @Component({
@@ -7,5 +8,15 @@ import { SharedDataService } from 'src/app/shared/shared-data.service';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent {
+
+  constructor(private SharedDataService: SharedDataService){}
+
+  ngOnInit(){
+    //me suscribo al servicio para recibir eventos 
+    this.SharedDataService.event$.subscribe(event => {
+
+      console.log(event)
+    })
+  }
 
 }
