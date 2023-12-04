1207ade438b17c17239aa1f0bfdd0ff36f27d7d7
tomasin <vendogarrafapuente83@gmail.com>
2023-12-04 20:16:53 -0300

    Creada la función para registrar al usuario

---
+++ src/servicios/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {GoogleAuthProvider} from '@angular/fire/auth'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private afs: AngularFireAuth) { }
+
+  signInwithGoogle(){
+    return this.afs.signInWithPopup(new GoogleAuthProvider());
+  }
+
+  registerWithEmailAndPassword(user: {email: string, password: string}){
+    return this.afs.createUserWithEmailAndPassword(user.email, user.password);
+  }
+}
