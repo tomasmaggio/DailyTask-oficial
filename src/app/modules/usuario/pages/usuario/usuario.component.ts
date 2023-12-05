7fd42261a3b0fe87d288b6bf8287f4885035e95a
tomasin <vendogarrafapuente83@gmail.com>
2023-12-05 15:27:14 -0300

    Ya se muestra nombre de usuario en vista de usuario y puede guardar, editar o borrar su descripción.

--- src/app/modules/usuario/pages/usuario/usuario.component.ts
+++ src/app/modules/usuario/pages/usuario/usuario.component.ts
@@ -1,29 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AuthService } from 'src/servicios/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
   styleUrls: ['./usuario.component.css']
 })
-export class UsuarioComponent {
+export class UsuarioComponent implements OnInit {
   mostrarBotones: boolean = false;
-  username : string = '';
+  username: string = '';
+  descripcion: string = '';
+  descripcioneditada: string = '';
+  textoOriginal: string = '';
 
   constructor(
     private authService: AuthService,
-    private afAuth: AngularFireAuth
+    private afAuth: AngularFireAuth,
+    private firestore: AngularFirestore
   ) {}
 
-  ngOnInit() {
-    this.afAuth.authState.subscribe(user => {
+  async ngOnInit() {
+    this.afAuth.authState.subscribe(async user => {
       if (user) {
-        const userId = user.uid;
-        console.log('ID del usuario:', userId); // Imprime el ID del usuario en la consola
+        const userId = (await user)?.uid;
+        console.log('ID del usuario:', userId);
         this.authService.getUsername(userId).subscribe(username => {
           this.username = username;
         });
+
+        this.firestore
+          .collection('users')
+          .doc(userId)
+          .valueChanges()
+          .subscribe((data: any) => {
+            this.descripcion = data?.descripcion || '';
+            this.descripcioneditada = this.descripcion;
+            this.textoOriginal = this.descripcion;
+          });
       }
     });
   }
@@ -32,7 +48,30 @@ export class UsuarioComponent {
     this.mostrarBotones = !this.mostrarBotones;
   }
 
-  editarImagen (){
-    console.log("edicion funcionando")
+  editarImagen() {
+    console.log('edicion funcionando');
+    // Aquí puedes implementar la lógica para cambiar la foto de usuario y guardarla en la base de datos
+  }
+
+  editarDescripcion() {
+    this.mostrarBotones = true;
+  }
+
+  async guardarCambios() {
+    this.descripcion = this.descripcioneditada;
+    const user = await this.afAuth.currentUser;
+    const userId = user?.uid;
+    if (userId) {
+      this.firestore.collection('users').doc(userId).update({
+        descripcion: this.descripcion
+      });
+    }
+    this.mostrarBotones = false;
+    this.textoOriginal = this.descripcion;
+  }
+
+  descartarCambios() {
+    this.descripcioneditada = this.textoOriginal;
+    this.mostrarBotones = false;
   }
 }
