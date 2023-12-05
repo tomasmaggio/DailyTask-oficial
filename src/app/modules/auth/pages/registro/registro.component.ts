0985eacef8c89c9ee8f3b72741133b4e6edb3909
tomasin <vendogarrafapuente83@gmail.com>
2023-12-04 22:05:35 -0300

    Agregados comentarios para mejorar la comprensión del código de firebase.

--- src/app/modules/auth/pages/registro/registro.component.ts
+++ src/app/modules/auth/pages/registro/registro.component.ts
@@ -7,35 +7,42 @@ import { AuthService } from 'src/servicios/auth.service';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-email: any;
-password: string;
+  email: any;
+  password: string;
 
-  constructor(private auth : AuthService){
-    
+  constructor(private auth: AuthService) {
+    // Inyectamos el servicio AuthService en el constructor del componente
   }
 
   ngOnInit(): void {
-
+    // Método del ciclo de vida del componente ngOnInit
   }
 
+  // Método para validar el formato del correo electrónico
   validateEmail(email: string): boolean {
     // Expresión regular para validar el formato del correo electrónico
     const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
     return emailRegex.test(email);
   }
+
+  // Método para registrar un nuevo usuario
   register() {
+    // Validamos el formato del correo electrónico
     if (!this.validateEmail(this.email)) {
       alert('Por favor ingrese un email válido.');
       return;
     }
 
-
+    // Validamos que se haya ingresado una contraseña
     if (this.password == '') {
       alert('Por favor ingrese una contraseña válida');
       return;
-    } // Lógica adicional para realizar el inicio de sesión
+    }
 
+    // Llamamos al método de registro del AuthService, pasando el correo electrónico y la contraseña
     this.auth.register(this.email, this.password);
+
+    // Reiniciamos los valores de email y password
     this.email = '';
     this.password = '';
   }
--- src/servicios/auth.service.ts
+++ src/servicios/auth.service.ts
@@ -10,37 +10,49 @@ export class AuthService {
 
   constructor(private fireauth: AngularFireAuth, private router: Router) { }
 
-//login 
-login(email: string, password: string) {
-  this.fireauth.signInWithEmailAndPassword(email, password).then(() => {
-    localStorage.setItem('token', 'true');
-    this.router.navigate(['/inicio']);
-  }, err => {
-    alert(err.message);
-    this.router.navigate(['/login']);
-  });
-}
-  // registro
+  // Método para realizar el inicio de sesión
+  login(email: string, password: string) {
+    // Utilizamos el servicio AngularFireAuth para autenticar al usuario
+    this.fireauth.signInWithEmailAndPassword(email, password).then(() => {
+      // Si el inicio de sesión es exitoso, guardamos un token en el localStorage
+      localStorage.setItem('token', 'true');
+      // Redirigimos al usuario a la página de inicio
+      this.router.navigate(['/inicio']);
+    }, err => {
+      // Si ocurre un error, mostramos una alerta y redirigimos al usuario a la página de inicio de sesión
+      alert(err.message);
+      this.router.navigate(['/login']);
+    });
+  }
+
+  // Método para realizar el registro de un nuevo usuario
   register(email: string, password: string) {
+    // Utilizamos el servicio AngularFireAuth para crear un nuevo usuario en Firebase
     this.fireauth.createUserWithEmailAndPassword(email, password)
       .then(() => {
-        alert('Creaste tu cuenta con éxito')
+        // Si el registro es exitoso, mostramos una alerta y redirigimos al usuario a la página de inicio de sesión
+        alert('Creaste tu cuenta con éxito');
         this.router.navigate(['/login']);
       })
       .catch((err) => {
+        // Si ocurre un error, mostramos un mensaje de error en la consola y redirigimos al usuario a la página de registro
         console.error(err);
         this.router.navigate(['/registro']);
       });
   }
 
-//Cerrar sesión
-logout() {
-  this.fireauth.signOut().then(() => {
-    localStorage.removeItem('token');
-    this.router.navigate(['/login']);
-  }).catch(err => {
-    alert(err.message);
-  });
-}
+  // Método para cerrar la sesión del usuario actual
+  logout() {
+    // Utilizamos el servicio AngularFireAuth para cerrar la sesión
+    this.fireauth.signOut().then(() => {
+      // Si el cierre de sesión es exitoso, eliminamos el token del localStorage
+      localStorage.removeItem('token');
+      // Redirigimos al usuario a la página de inicio de sesión
+      this.router.navigate(['/login']);
+    }).catch(err => {
+      // Si ocurre un error, mostramos una alerta con el mensaje de error
+      alert(err.message);
+    });
+  }
 
 }
