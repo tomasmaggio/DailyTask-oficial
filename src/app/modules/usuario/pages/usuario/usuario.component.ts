d1225a22926438d26b381ddb266de2b7bb064791
tomasin <vendogarrafapuente83@gmail.com>
2023-12-05 21:36:20 -0300

    Los usuarios pueden modificar su descripción.

--- src/app/modules/usuario/pages/usuario/usuario.component.ts
+++ src/app/modules/usuario/pages/usuario/usuario.component.ts
@@ -2,13 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AuthService } from 'src/servicios/auth.service';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
-import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
-  styleUrls: ['./usuario.component.css']
+  styleUrls: ['./usuario.component.css'],
 })
 export class UsuarioComponent implements OnInit {
   mostrarBotones: boolean = false;
@@ -23,12 +21,12 @@ export class UsuarioComponent implements OnInit {
     private firestore: AngularFirestore
   ) {}
 
-  async ngOnInit() {
-    this.afAuth.authState.subscribe(async user => {
+  ngOnInit() {
+    this.afAuth.authState.subscribe(async (user) => {
       if (user) {
         const userId = (await user)?.uid;
         console.log('ID del usuario:', userId);
-        this.authService.getUsername(userId).subscribe(username => {
+        this.authService.getUsername(userId).subscribe((username) => {
           this.username = username;
         });
 
@@ -49,11 +47,6 @@ export class UsuarioComponent implements OnInit {
     this.mostrarBotones = !this.mostrarBotones;
   }
 
-  editarImagen() {
-    console.log('edicion funcionando');
-    // Aquí puedes implementar la lógica para cambiar la foto de usuario y guardarla en la base de datos
-  }
-
   editarDescripcion() {
     this.mostrarBotones = true;
   }
@@ -64,7 +57,7 @@ export class UsuarioComponent implements OnInit {
     const userId = user?.uid;
     if (userId) {
       this.firestore.collection('users').doc(userId).update({
-        descripcion: this.descripcion
+        descripcion: this.descripcion,
       });
     }
     this.mostrarBotones = false;
