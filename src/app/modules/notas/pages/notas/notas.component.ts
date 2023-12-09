6d206a608bcb0d587d2a8336b782315f05f81f17
tomasin <vendogarrafapuente83@gmail.com>
2023-12-09 04:11:23 -0300

    Solucionados errores de síntaxis

--- src/app/modules/notas/pages/notas/notas.component.ts
+++ src/app/modules/notas/pages/notas/notas.component.ts
@@ -28,14 +28,14 @@ export class NotasComponent implements OnInit {
 
   //Borrar una nota
   borrarNota(nota: Notas){
-    let docRef = doc(collection(this.afs,'notas/${nota.id}'))
+    let docRef = doc(this.afs,'notas/${nota.id}')
   
     return deleteDoc(docRef)
   }
 
   //Editar una nota
   editarNota(nota: Notas, notas: any){
-    let docRef = doc(collection(this.afs,'notas/${nota.id}'));
+    let docRef = doc(this.afs,'notas/${nota.id}');
   }
   ngOnInit(): void {
     // Aquí puedes agregar la lógica de inicialización del componente
