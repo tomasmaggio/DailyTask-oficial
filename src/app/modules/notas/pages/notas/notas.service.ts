1dadfc963d8677c50a78275e2d2029220430a2b9
tomasin <vendogarrafapuente83@gmail.com>
2023-12-09 04:16:43 -0300

    Migrados los cambios necesarios a notas.service.ts

---
+++ src/app/modules/notas/pages/notas/notas.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Notas } from 'src/app/models/notas';
+import { Firestore, addDoc, collection, collectionData, deleteDoc, doc, updateDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotasService {
+
+  constructor(private afs: Firestore) { }
+
+  // Añadir una nueva nota
+  añadirNota(nota: Notas) {
+    nota.id = doc(collection(this.afs, 'id')).id;
+    return addDoc(collection(this.afs, 'notas'), nota);
+  }
+
+  // Obtener todas las notas
+  obtenerNotas(): Observable<Notas[]> {
+    let notasRef = collection(this.afs, 'notas');
+    return collectionData(notasRef, { idField: 'id' }) as Observable<Notas[]>;
+  }
+
+  // Borrar una nota
+  borrarNota(nota: Notas) {
+    let docRef = doc(this.afs, `notas/${nota.id}`);
+    return deleteDoc(docRef);
+  }
+
+  // Editar una nota
+  editarNota(nota: Notas, notas: any) {
+    let docRef = doc(this.afs, `notas/${nota.id}`);
+    return updateDoc(docRef, notas);
+  }
+}
