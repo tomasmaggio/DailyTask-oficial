f4740007d3891ca640eef8963c7fc54852450b3d
tomasin <vendogarrafapuente83@gmail.com>
2023-12-09 04:18:29 -0300

    Agregados imports en notas.component.ts

--- src/app/modules/notas/pages/notas/notas.component.ts
+++ src/app/modules/notas/pages/notas/notas.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Notas } from 'src/app/models/notas';
-import { Firestore, addDoc, collection, collectionData, deleteDoc, doc, updateDoc } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-notas',
@@ -10,34 +8,8 @@ import { Observable } from 'rxjs';
 })
 export class NotasComponent implements OnInit {
 
-  constructor(private afs: Firestore) { }
+  constructor() { }
 
-
-  //Añadir una nueva nota
-  añadirNota(nota: Notas){
-    nota.id = doc(collection(this.afs,'id')).id
-    return addDoc(collection(this.afs,'notas'),nota)
-  }
-
-  //Obtener todas las notas
-  obtenerNotas(): Observable<Notas[]>{
-    let notasRef = collection(this.afs, 'notas')
-    return collectionData(notasRef,{idField:'id'} )as
-    Observable<Notas[]>
-  }
-
-  //Borrar una nota
-  borrarNota(nota: Notas){
-    let docRef = doc(this.afs,'notas/${nota.id}')
-  
-    return deleteDoc(docRef)
-  }
-
-  //Editar una nota
-  editarNota(nota: Notas, notas: any){
-    let docRef = doc(this.afs,'notas/${nota.id}');
-    return updateDoc(docRef,notas)
-  }
   ngOnInit(): void {
     // Aquí puedes agregar la lógica de inicialización del componente
   }
